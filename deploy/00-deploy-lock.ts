6f97b1cbff3d56fef9f4fab1d6ec7e54036d6353
Santiago Viana <salviega6@gmail.com>
2024-06-10 09:53:15 -0500

    :technologist: Improving developer experience

--- deploy/00-deploy-lock.ts
+++ deploy/00-deploy-lock.ts
@@ -1,28 +1,30 @@
-import { DeployFunction, DeployResult } from "hardhat-deploy/dist/types";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { networkConfig } from "../helper-hardhat-config.ts";
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction /* DeployResult */ } from 'hardhat-deploy/dist/types'
+
+import { networkConfig } from '../helper-hardhat-config.ts'
 
 const deployLock: DeployFunction = async function (
-  hre: HardhatRuntimeEnvironment
+	hre: HardhatRuntimeEnvironment
 ) {
-  const { getNamedAccounts, deployments, network } = hre;
-  const { deploy, log } = deployments;
-  const { deployer } = await getNamedAccounts();
+	const { getNamedAccounts, deployments, network } = hre
+	const { deploy, log } = deployments
+	const { deployer } = await getNamedAccounts()
 
-  const currentTime: number = Math.floor(Date.now() / 1000);
-  const unlockTime: number = currentTime + 60;
+	const currentTime: number = Math.floor(Date.now() / 1000)
+	const unlockTime: number = currentTime + 60
 
-  const args: any[] = [unlockTime];
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const args: any[] = [unlockTime]
 
-  log("-----------------------------------");
-  log("Deploying Lock...");
+	log('-----------------------------------')
+	log('Deploying Lock...')
 
-  const lock: DeployResult = await deploy("Lock", {
-    from: deployer,
-    args: args,
-    log: true,
-    waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
-  });
-};
+	/* const lock: DeployResult = */ await deploy('Lock', {
+		from: deployer,
+		args,
+		log: true,
+		waitConfirmations: networkConfig[network.name].blockConfirmations || 1
+	})
+}
 
-export default deployLock;
+export default deployLock
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,21 +1,21 @@
 export interface networkConfigItem {
-  ethUsdPriceFeed?: string;
-  blockConfirmations?: number;
+	ethUsdPriceFeed?: string
+	blockConfirmations?: number
 }
 
 export interface networkConfigInfo {
-  [key: string]: networkConfigItem;
+	[key: string]: networkConfigItem
 }
 
 export const networkConfig: networkConfigInfo = {
-  hardhat: {},
-  localhost: {},
-  arbitrumOne: {
-    blockConfirmations: 3,
-  },
-  arbitrumSepolia: {
-    blockConfirmations: 3,
-  },
-};
+	hardhat: {},
+	localhost: {},
+	arbitrumOne: {
+		blockConfirmations: 3
+	},
+	arbitrumSepolia: {
+		blockConfirmations: 3
+	}
+}
 
-export const developmentChains = ["hardhat", "localhost"];
+export const developmentChains = ['hardhat', 'localhost']
