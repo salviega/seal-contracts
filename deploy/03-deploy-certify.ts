d736faf877f5e0fd7617dc836ee5c23d226c5548
Santiago Viana <salviega6@gmail.com>
2024-06-21 11:40:38 -0500

    :zap: Others deployments

--- deploy/03-deploy-certify.ts
+++ deploy/03-deploy-certify.ts
@@ -11,24 +11,24 @@ import {
 import { saveUpgradeableContractDeploymentInfo } from '../helpers/upgrades/save-upgradeable-contract-deployment-info.ts'
 import { verify } from '../helpers/verify.ts'
 
-const deployRegistry: DeployFunction = async function (
+const deployCertify: DeployFunction = async function (
 	hre: HardhatRuntimeEnvironment
 ) {
 	const { getNamedAccounts, deployments, network } = hre
 	const { log, get, save } = deployments
 	const { deployer } = await getNamedAccounts()
 
-	const sp: Deployment = await get('SP')
+	const registry: Deployment = await get('Registry')
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const args: any[] = [deployer, sp.address]
+	const args: any[] = [deployer, registry.address, deployer]
 
 	log('-----------------------------------')
-	log('Deploying Registry...')
+	log('Deploying Certify...')
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const Registry: ContractFactory<any[], Contract> =
-		await ethers.getContractFactory('Registry')
+		await ethers.getContractFactory('Certify')
 
 	const proxy: Contract = await upgrades.deployProxy(Registry, args)
 	await proxy.waitForDeployment()
@@ -40,27 +40,27 @@ const deployRegistry: DeployFunction = async function (
 		throw new Error('No deployment transaction found')
 	}
 
-	log(`Regsitry transaction hash: ${proxyTransaction.hash}`)
+	log(`Certify transaction hash: ${proxyTransaction.hash}`)
 
 	const proxyAddress: string = await proxy.getAddress()
-	log(`Registry proxy deployed at: ${proxyAddress}`)
+	log(`Certify proxy deployed at: ${proxyAddress}`)
 
 	const implementationAddress: string =
 		await getImplementationAddress(proxyAddress)
-	log(`Registry implementation deployed at: ${implementationAddress}`)
+	log(`Certify implementation deployed at: ${implementationAddress}`)
 
 	const proxyAdmin: string = await getProxyAdmin(proxyAddress)
-	log(`Registry proxy admin: ${proxyAdmin}`)
+	log(`Certify proxy admin: ${proxyAdmin}`)
 
 	if (!developmentChains.includes(network.name)) {
 		await verify(proxyAddress, [])
 	}
 
-	const artifact = await deployments.getExtendedArtifact('Registry')
-	await save('Registry', { address: proxyAddress, ...artifact })
+	const artifact = await deployments.getExtendedArtifact('Certify')
+	await save('Certify', { address: proxyAddress, ...artifact })
 
-	await saveUpgradeableContractDeploymentInfo('Registry', proxy)
+	await saveUpgradeableContractDeploymentInfo('Certify', proxy)
 }
 
-deployRegistry.tags = ['all', 'Registry']
-export default deployRegistry
+deployCertify.tags = ['all', 'Certify']
+export default deployCertify
---
+++ deploy/04-deploy-course.ts
@@ -0,0 +1,45 @@
+import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction, Deployment } from 'hardhat-deploy/dist/types'
+
+import { developmentChains, networkConfig } from '../helper-hardhat-config'
+import { verify } from '../helpers/verify'
+
+const deployCourse: DeployFunction = async function (
+	hre: HardhatRuntimeEnvironment
+) {
+	const { getNamedAccounts, deployments, network, ethers } = hre
+	const { log, get, deploy } = deployments
+	const { deployer } = await getNamedAccounts()
+
+	const deployerSigner: HardhatEthersSigner = await ethers.getSigner(deployer)
+	const certifyDeployment: Deployment = await get('Certify')
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const args: any[] = ['', '', certifyDeployment.address]
+
+	log('-----------------------------------')
+	log('Deploying Course...')
+
+	const courseDeployment = await deploy('Course', {
+		from: deployer,
+		args,
+		log: true,
+		waitConfirmations: networkConfig[network.name].blockConfirmations || 1
+	})
+
+	if (!developmentChains.includes(network.name)) {
+		await verify('Course', args)
+	}
+
+	const certify = await ethers.getContractAt(
+		'Certify',
+		certifyDeployment.address,
+		deployerSigner
+	)
+
+	await certify.addToCloneableCourse(courseDeployment.address)
+}
+
+deployCourse.tags = ['all', 'Course']
+export default deployCourse
